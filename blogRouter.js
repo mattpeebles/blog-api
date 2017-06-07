7e40bb93ff9280fc380231cd805362374e7d6558
Matthew Peebles <peebles.mm@gmail.com>
2017-06-07 10:57:43 -0500

    Updated models

--- blogRouter.js
+++ blogRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 const router = express.Router()
-const {Blog} = require('models')
+const {Blog} = require('./models')
 
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json();
